51b2df5fcad93eed2f96ef0ca1923f97fec5116c
Thevihn5804 <thevinh58.o4@gmail.com>
2025-07-02 17:53:43 +0700

    Create_DemoMVC_Models

--- DEMOMVC/Models/DaiLy.cs
+++ DEMOMVC/Models/DaiLy.cs
@@ -1,6 +1,6 @@
 namespace DEMOMVC.Models
 {
-    public class DaiLy : HeThongPhanPhoi
+    public class DaiLy
     {
         public string? MaDaiLy { get; set; }
         public string? TenDaiLy { get; set; }
@@ -8,5 +8,6 @@ namespace DEMOMVC.Models
         public string? NguoiDaiDien { get; set; }
         public string? DienThoai { get; set; }
         public string? MaHTPP { get; set; }
+        public HeThongPhanPhoi? HeThongPhanPhoi { get; set; }
     }
 }
